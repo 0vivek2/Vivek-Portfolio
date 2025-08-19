fc66fdc885f85186663591ad998652a457bdf0fb
VIVEK <vkvillain2000@gmail.com>
2025-08-19 17:37:26 +0530

    Changed Again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Vivek-Portfolio/",
+ 
   plugins: [react()],
+   base: "/Vivek-Portfolio/",
 })
